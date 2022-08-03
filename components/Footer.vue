d721940ac67c3a6df7477c794606997f10d432e8
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-03 14:18:43 +0400

    as

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,33 @@
 <template>
     <footer>
-        
+        <div class="flex justify-around items-center  w-full ">
+            <div class="">
+                <img src="../assets/logo .svg" alt="">
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Central street, White City, Khatai, Baku, Azerbaijan</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">info@izzi.com</p>
+            </div>
+            <div>
+                <p>izzi Ltd.</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Location adres</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">info@izzi.com</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">NR21465161</p>
+            </div>
+            <div>
+                <p>Book a service</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Browse Services</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Find Taskers</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">How does it work?</p>
+            </div>
+            <div>
+                <p>Extra links</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">About us</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">FAQ</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Contact us</p>
+                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Become a Task</p>
+            </div>
+        </div>
     </footer>
 </template>
 
