2ee081a32d79711315a6e6848e3fdfbe6f119a07
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-03 15:37:03 +0400

    asdads

--- components/NuxtLogo.vue
+++ components/NuxtLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container"><p class="text-5xl text-center">Murad & Nizami</p></div>
+  <div class="container h-[200vh]"><p class="text-5xl text-center">Murad & Nizami</p></div>
 </template>
 
 <style>
