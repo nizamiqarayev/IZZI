4d562c565b35a987b995e08d9db57f06470d129e
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-03 15:10:23 +0400

    d

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
     <header class="w-full flex justify-center items-center font-quicksand">
         <div class="w-full px-56 py-4 flex justify-between">
             <div class="flex ">
-                <img src="../assets/logo .svg" class="mr-12" alt="">
+                <img src="../assets./logo/logo.svg" class="mr-12" alt="">
                 <nav class="flex items-center gap-8 ">
                     <p class=" whitespace-nowrap">My Orders</p>
                     <p class=" whitespace-nowrap">Services</p>
@@ -13,7 +13,7 @@
             <div v-if="signedIn == false" class="flex items-center">
             <nuxt-link to="/signin" class="mr-8 text-[#5920BC]">Sign In</nuxt-link>
                         <nuxt-link to="/signin" class="bg-[#5920BC] flex gap-4 px-6 py-3 rounded-[3px] text-white">
-                            <img src="../assets/User.svg" alt="">
+                            <img src="../assets/logo/User.svg" alt="">
                             <p class="">Sign up</p></nuxt-link>
 
             </div>
