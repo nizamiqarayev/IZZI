14f0ae3885005bff46ba5abb88627ed9668d35d6
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-03 14:41:02 +0400

    a

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,26 +1,26 @@
 <template>
     <footer>
-        <div class="flex  justify-around items-center h-[200rem] w-full ">
+        <div class="flex justify-around items-center px-28 py-12 w-full ">
             <div class="">
                 <img src="../assets/logo .svg" alt="">
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">Central street, White City, Khatai, Baku, Azerbaijan</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">info@izzi.com</p>
             </div>
-            <div>
+            <div class="">
                 <p>izzi Ltd.</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">Location adres</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">info@izzi.com</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">NR21465161</p>
             </div>
-            <div>
+            <div class="">
                 <p>Book a service</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">Browse Services</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">Find Taskers</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">How does it work?</p>
             </div>
-            <div>
+            <div class="">
                 <p>Extra links</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">About us</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">FAQ</p>
