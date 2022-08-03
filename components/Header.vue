a0d240fbc40ed0959dfdfd479e6b3be4d3353b44
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-03 13:55:12 +0400

    s

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,16 @@
 <template>
   <header>
     <img src="../assets/logo .svg" alt="">
+    <nav><p>My Orders</p>
+    <p>Services</p>
+    <p>Help</p></nav>
   </header>
 </template>
 
 <script>
 export default {
     name: "Header",
-    
+
 }
 </script>
 
