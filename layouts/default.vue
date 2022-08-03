76c55a84a64f8e3a53fb96597d5daedf3e2aa663
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-03 13:47:42 +0400

    a

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,19 @@
 <template>
     <div>
-        
+    <Header></Header>
     <Nuxt/>
+    <Footer></Footer>
     </div>
 </template>
 
 <script>
+import Footer from "../components/Footer.vue";
+import Header from "../components/Header.vue";
 
+export default{
+    components:{
+        Footer,
+        Header
+    }
+}
 </script>
