4c0e08d343e0ca15e46b676e879a9056fa13aa1c
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-03 15:04:57 +0400

    asas

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,31 +1,36 @@
 <template>
     <footer>
-        <div class="flex justify-between items-center px-28 py-12 w-full ">
-            <div class="w-auto space-y-6">
+        <div class="flex justify-between px-28 py-12 w-full ">
+            <div class="px-8 pb-6 w-[300px] space-y-3">
                 <img src="../assets/Logo_footer.svg" alt="">
-                <h3 class="text-[#222222] text-base opacity-75 cursor-pointer">Central street, White City, Khatai, Baku, Azerbaijan</h3>
+                <span class="text-[#222222] text-base opacity-75 cursor-pointer">Central street, White City, Khatai, Baku, Azerbaijan</span>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">info@izzi.com</p>
             </div>
             <div class=" space-y-6">
-                <p>izzi Ltd.</p>
+                <p class="font-bold">izzi Ltd.</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">Location adres</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">info@izzi.com</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">NR21465161</p>
             </div>
             <div class=" space-y-6">
-                <p>Book a service</p>
+                <p class="font-bold">Book a service</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">Browse Services</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">Find Taskers</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">How does it work?</p>
             </div>
             <div class=" space-y-6">
-                <p>Extra links</p>
+                <p class="font-bold">Extra links</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">About us</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">FAQ</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">Contact us</p>
                 <p class="text-[#222222] text-base opacity-75 cursor-pointer">Become a Task</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                
             </div>
         </div>
     </footer>
