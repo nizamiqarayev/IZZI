95dfd07df551e9c2ee02b831705f94ae454435f5
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-03 13:52:31 +0400

    commit

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <img src="../assets/logo .svg" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    
+}
+</script>
+
+<style>
+
+</style>
