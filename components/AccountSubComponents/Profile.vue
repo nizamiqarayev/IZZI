d0c6ccea4d4eb054bf954c0febd38630320450d7
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-04 16:45:19 +0400

    a

---
+++ components/AccountSubComponents/Profile.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="flex items-center w-full bg-white px-8">
+        <div class="flex items-center border-b border-b-gray ">
+            <h2 class="mr-44 font-bold text-2xl pt-6 pb-4 ">Profile</h2>
+            <div class="flex items-center gap-8">
+                <div class="flex items-center gap-3 ">
+                    <p :class="onlineStatus ? 'text-gray-300' : 'text-[#5920BC]'">offline</p>
+                    <div class="h-5 rounded-full w-8 px-1 hover:cursor-pointer border border-[#5920BC] flex items-center transition-all duration-100 "
+                        :class="onlineStatus ? 'justify-end' : 'justify-start'" @click="onlineStatus = !onlineStatus"><img
+                            class="transition-all" src="../../assets/images/accounticons/Vector.svg" alt=""></div>
+                    <p :class="onlineStatus ? 'text-[#5920BC]' : 'text-gray-300'">online</p>
+                </div>
+                <div class="flex items-center gap-2 text-[#5920BC]">
+                    <img src="../../assets/images/accounticons/edit.svg" alt="">
+                    <p>Edit profile</p>
+                </div>
+                <img src="../../assets/images/accounticons/Exit.svg" alt="">
+            </div>
+        </div>
+        <div class="pt-8">
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            onlineStatus: true,
+        }
+    },
+}
+</script>
+
+<style>
+</style>
--- components/Header.vue
+++ components/Header.vue
@@ -2,40 +2,42 @@
     <header class="w-full flex justify-center items-center font-quicksand shadow-inner fixed bg-white z-40 h-20 ">
         <div class="w-full px-56 py-4 flex justify-between">
             <div class="flex ">
-                <img src="../assets/images/logo/logo .svg" class="mr-12 hover:cursor-pointer " alt="">
+                <nuxt-link to="/"> <img src="../assets/images/logo/logo .svg" class="mr-12 hover:cursor-pointer "
+                        alt="">
+                </nuxt-link>
                 <nav class="flex items-center gap-8 text-sm">
                     <p class="hover:text-[#5920BC] hover: hover:cursor-pointer whitespace-nowrap">My Orders</p>
                     <p class="hover:text-[#5920BC] hover:cursor-pointer whitespace-nowrap">Services</p>
                     <p class="hover:text-[#5920BC] hover:cursor-pointer whitespace-nowrap">Help</p>
                 </nav>
             </div>
-<<<<<<< HEAD
             <div v-if="headerstate == true">
                 <nuxt-link to="Account">
                     <div class="flex items-center gap-3">
                         <img src="../assets/images/headerpfp/Oval.svg" alt="">
-                        <p class="flex items-center hover:text-[#5920BC] h-8 px-3  rounded-md border border-transparent hover:border-gray-200 hover:shadow-inner ">Elon Musk</p>
+                        <p
+                            class="flex items-center hover:text-[#5920BC] h-8 px-3  rounded-md border border-transparent hover:border-gray-200 hover:shadow-inner ">
+                            Elon Musk</p>
                     </div>
                 </nuxt-link>
             </div>
             <div v-if="headerstate == false" class="flex items-center">
                 <nuxt-link to="/signin" class="mr-8 text-[#5920BC] hover:text-[#220c48]">Sign In</nuxt-link>
-                <nuxt-link to="/signin"
+                <nuxt-link to="/signup"
                     class="bg-[#5920BC] hover:bg-[#220c48] flex gap-4 px-6 py-3 rounded-[3px] text-white">
                     <img src="../assets/images/logo/User.svg" alt="">
                     <p class="">Sign up</p>
                 </nuxt-link>
-=======
-            <div v-if="signedIn == true"></div>
+                <!-- <div v-if="signedIn == true"></div>
             <div v-if="signedIn == false" class="flex items-center">
             <nuxt-link to="/signin" class="mr-8 text-[#5920BC] hover:text-[#220c48]">Sign In</nuxt-link>
                         <nuxt-link to="/signup" class="bg-[#5920BC] hover:bg-[#220c48] flex gap-4 px-6 py-3 rounded-[3px] text-white">
                             <img src="../assets/images/logo/User.svg" alt="">
                             <p class="">Sign up</p></nuxt-link>
->>>>>>> 8df1ff36333838884f55eca985fb34f9d6456b7a
+
+            </div> -->
 
             </div>
-
         </div>
     </header>
 </template>
