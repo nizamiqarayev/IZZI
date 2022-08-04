ae56ba25027b4cf7a59971ef38538a8909c15d2c
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-04 15:14:44 +0400

    commit

--- components/Header.vue
+++ components/Header.vue
@@ -9,12 +9,21 @@
                     <p class="hover:text-[#5920BC] hover:cursor-pointer whitespace-nowrap">Help</p>
                 </nav>
             </div>
-            <div v-if="signedIn == true"></div>
-            <div v-if="signedIn == false" class="flex items-center">
-            <nuxt-link to="/signin" class="mr-8 text-[#5920BC] hover:text-[#220c48]">Sign In</nuxt-link>
-                        <nuxt-link to="/signin" class="bg-[#5920BC] hover:bg-[#220c48] flex gap-4 px-6 py-3 rounded-[3px] text-white">
-                            <img src="../assets/images/logo/User.svg" alt="">
-                            <p class="">Sign up</p></nuxt-link>
+            <div v-if="headerstate == true">
+                <nuxt-link to="Account">
+                    <div class="flex items-center gap-3">
+                        <img src="../assets/images/headerpfp/Oval.svg" alt="">
+                        <p class="flex items-center hover:text-[#5920BC] h-8 px-3  rounded-md border border-transparent hover:border-gray-200 hover:shadow-inner ">Elon Musk</p>
+                    </div>
+                </nuxt-link>
+            </div>
+            <div v-if="headerstate == false" class="flex items-center">
+                <nuxt-link to="/signin" class="mr-8 text-[#5920BC] hover:text-[#220c48]">Sign In</nuxt-link>
+                <nuxt-link to="/signin"
+                    class="bg-[#5920BC] hover:bg-[#220c48] flex gap-4 px-6 py-3 rounded-[3px] text-white">
+                    <img src="../assets/images/logo/User.svg" alt="">
+                    <p class="">Sign up</p>
+                </nuxt-link>
 
             </div>
 
@@ -23,16 +32,33 @@
 </template>
 
 <script>
+import { mapState, mapActions, mapGetters, mapMutations } from 'vuex';
 export default {
     name: "Header",
     props: {
-        signedInProp:Boolean,
+        signedInProp: Boolean,
     },
     data() {
         return {
-            signedIn:false,
+            signedIn: this.$store.getters["headerstore/getState"],
         }
     },
+    methods: {
+        ...mapGetters('headerstore', ['getState'])
+    },
+
+    computed: {
+        headerstate() {
+            console.log('====================================');
+            console.log(this.signedIn);
+            console.log('====================================');
+            return this.$store.getters["headerstore/getState"]
+        }
+    },
+    mounted() {
+        console.log("getter: " + this.getState);
+    },
+
 
 }
 </script>
