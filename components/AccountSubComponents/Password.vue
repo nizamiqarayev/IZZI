cf813cd588a5b35d943ebc7a64b4b878620d003a
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-05 13:52:14 +0400

    asdad

--- components/AccountSubComponents/Password.vue
+++ components/AccountSubComponents/Password.vue
@@ -5,22 +5,22 @@
         </div>
         <div>
             <form>
-                <div class="flex items-center border-b mt-4 pb-4">
+                <div class="flex items-center justify-between border-b mt-4 pb-4">
                     <label class="whitespace-nowrap">Enter current password</label>
                     <input
-                        class=" ml-32 border-[#C7C9CB] w-2/5 border-[1px] focus:border-purple-500 focus:outline-none py-2 rounded-lg px-6 placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]"
+                        class=" mr-32 border-[#C7C9CB] w-2/5 border-[1px] focus:border-purple-500 focus:outline-none py-2 rounded-lg px-6 placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]"
                         type="password">
                 </div>
-                <div class="flex items-center border-b mt-4 pb-4">
+                <div class="flex items-center justify-between border-b mt-4 pb-4">
                     <label class="whitespace-nowrap">Enter new password</label>
                     <input
-                        class=" ml-32 border-[#C7C9CB] w-2/5 border-[1px] focus:border-purple-500 focus:outline-none py-2 rounded-lg px-6 placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]"
+                        class=" mr-32 border-[#C7C9CB] w-2/5 border-[1px] focus:border-purple-500 focus:outline-none py-2 rounded-lg px-6 placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]"
                         type="password">
                 </div>
-                <div class="flex items-center border-b mt-4 pb-4">
+                <div class="flex items-center justify-between border-b mt-4 pb-4">
                     <label class="whitespace-nowrap">Confirm new password</label>
                     <input
-                        class=" ml-32 border-[#C7C9CB] w-2/5 border-[1px] focus:border-purple-500 focus:outline-none py-2 rounded-lg px-6 placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]"
+                        class=" mr-32 border-[#C7C9CB] w-2/5 border-[1px] focus:border-purple-500 focus:outline-none py-2 rounded-lg px-6 placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]"
                         type="password">
                 </div>
                 <div class="flex items-center justify-end gap-4 py-4">
