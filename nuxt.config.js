3ec925d42fc2a105ed48aab11fa69809e656218d
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-05 15:57:57 +0400

    dad

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,6 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/vuetify',
     '@nuxtjs/tailwindcss',
     '@nuxt/postcss8',
   ],
