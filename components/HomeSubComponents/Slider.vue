39312c7a8588a7ec70a262e86d3d62f1f5579077
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-05 17:34:42 +0400

    kgku

--- components/HomeSubComponents/Slider.vue
+++ components/HomeSubComponents/Slider.vue
@@ -1,16 +1,88 @@
 <template>
-  <div class="max-h-screen">
-    <SliderItemVue  />
+  <div class="h-screen flex items-center justify-center">
+    <div class="">
+      <slick class="max-w-4/5 flex flex-row justify-center items-center max-w-3xl" ref="slick" :options="slickOptions">
+        <SliderItemVue />
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+        <div>
+          <SliderItemVue />
+        </div>
+
+      </slick>
+
     </div>
+
+
+
+  </div>
 </template>
 
 <script>
 import SliderItemVue from './SliderItem.vue'
+import 'slick-carousel/slick/slick.css';
+
+import Slick from 'vue-slick'
+// import { VueSlickCarousel } from 'vue-slick-carousel'
+// import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+
 export default {
-components: {SliderItemVue,}
+  name: "Slider",
+  components: {
+    SliderItemVue,
+    Slick
+  },
+  data() {
+    return {
+      items: 9,
+      slickOptions: {
+        dots: false,
+        arrows: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+
+
+      }
+    }
+  },
 }
 </script>
 
 <style>
-
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,8 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+
+
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
