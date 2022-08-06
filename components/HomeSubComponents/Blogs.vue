5f657cf57d346357f8a7e022b9213177c5c962d1
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-06 15:37:51 +0400

    qeq

---
+++ components/HomeSubComponents/Blogs.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="font-quicksand mt-24">
+        <div class="flex justify-between items-center ">
+            <h3 class="text-3xl font-bold">Blogs</h3>
+            <nuxt-link to="services"
+                class="flex items-center gap-2 justify-center border rounded-full border-[#5920BC] py-3 px-6">
+                <p class="text-[#5920BC]"> Discover All Blogs</p>
+                <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 1L17 6M17 6H1M17 6L12 11" stroke="#5920BC" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </svg>
+
+
+            </nuxt-link>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
