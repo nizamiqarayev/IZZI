078d8c3639afc0cc3a536274cf745787891267fe
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-08 11:24:11 +0400

    asd

--- pages/Pros.vue
+++ pages/Pros.vue
@@ -3,13 +3,21 @@
         <main class="w-10/12 mx-auto py-40">
             <p class="text-[30px] font-bold ">All Pros</p>
             <div class="w-full pt-16">
-                <div>
-                    <div class="flex items-center border-8">
+                <div class="flex justify-between gap-14">
+                    <div class="flex items-center border-[1px] rounded relative w-3/5">
                         <label for="search">
-                            <img class="relative left-5 " src="../assets/images/search (3).svg" alt="">
+                            <img class="absolute bottom-3 left-3 z-10 " src="../assets/images/search (3).svg" alt="">
                         </label>
-                        <input type="text" class="px-10 py-2" id="search">
+                        <input type="text" class="relative px-10 py-2 w-full" id="search">
                     </div>
+                    <div class="flex gap-8 w-2/5">
+                        <div class="border-[1px] w-full rounded">
+                            <input type="text" class="w-full px-10 py-2">
+                        </div>
+                        <div class="border-[1px] w-full rounded">
+                            <input type="text" class="w-full px-10 py-2">
+                        </div>
+                    </div>  
                 </div>
             </div>
         </main>
