c02a44eddcdc63f80063481321d4b18f2c9cc340
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-08 16:37:47 +0400

    asas

--- pages/Pros.vue
+++ pages/Pros.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="bg-main bg-no-repeat bg-left-top bg-contain w-full">
+    <section class="bg-main font-quicksand bg-no-repeat bg-left-top bg-contain w-full">
         <main class="w-10/12 mx-auto py-40">
             <p class="text-[30px] font-bold ">All Pros</p>
             <div class="w-full pt-16">
@@ -17,15 +17,74 @@
                         <div class="border-[1px] w-full rounded">
                             <input type="text" class="w-full px-10 py-2">
                         </div>
-                    </div>  
+                    </div>
                 </div>
                 <div>
-                    <div>
-                        <div>
-                            <div class="flex" @click="show1=!show1">
-                                <img src="../assets/images/Squircle.svg" @click="show1=!show1" v-if="show1" alt="">
-                                <img src="../assets/images/Checked.svg" @click="show1=!show1" v-if="!show1"  alt="">
-                                <p >Cleaning</p>
+                    <div class="pt-9">
+                        <div class="flex flex-col gap-4">
+                            <div class="flex flex-col gap-2">
+                                <p class="text-[#6F6F6F] font-bold">Services</p>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterCleaning = !filterCleaning"
+                                        v-if="!filterCleaning" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterCleaning = !filterCleaning"
+                                        v-if="filterCleaning" alt="">
+                                    <p @click="filterCleaning = !filterCleaning">Cleaning</p>
+                                </div>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterHomeRepair = !filterHomeRepair"
+                                        v-if="!filterHomeRepair" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterHomeRepair = !filterHomeRepair"
+                                        v-if="filterHomeRepair" alt="">
+                                    <p @click="filterHomeRepair = !filterHomeRepair">Home Repair</p>
+                                </div>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterBeauty = !filterBeauty"
+                                        v-if="!filterBeauty" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterBeauty = !filterBeauty"
+                                        v-if="filterBeauty" alt="">
+                                    <p @click="filterBeauty = !filterBeauty">Beauty</p>
+                                </div>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterBabysitter = !filterBabysitter"
+                                        v-if="!filterBabysitter" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterBabysitter = !filterBabysitter"
+                                        v-if="filterBabysitter" alt="">
+                                    <p @click="filterBabysitter = !filterBabysitter">Babysitter</p>
+                                </div>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterWindowCleaning = !filterWindowCleaning" v-if="!filterWindowCleaning" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterWindowCleaning = !filterWindowCleaning" v-if="filterWindowCleaning" alt="">
+                                    <p @click="filterWindowCleaning=!filterWindowCleaning">Window cleaning</p>
+                                </div>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterSpecialCare = !filterSpecialCare" v-if="!filterSpecialCare" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterSpecialCare = !filterSpecialCare" v-if="filterSpecialCare" alt="">
+                                    <p @click="filterSpecialCare=!filterSpecialCare">Special Care</p>
+                                </div>
+                            </div>
+                            <div class="flex flex-col gap-2">
+                                <p class="text-[#6F6F6F] font-bold">By City</p>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterBaku = !filterBaku" v-if="!filterBaku" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterBaku = !filterBaku" v-if="filterBaku" alt="">
+                                    <p @click="filterBaku=!filterBaku">Baku</p>
+                                </div>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterSumgayit = !filterSumgayit" v-if="!filterSumgayit" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterSumgayit = !filterSumgayit" v-if="filterSumgayit" alt="">
+                                    <p @click="filterSumgayit=!filterSumgayit">Sumgayit</p>
+                                </div>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterQuba = !filterQuba" v-if="!filterQuba" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterQuba = !filterQuba" v-if="filterQuba" alt="">
+                                    <p @click="filterQuba=!filterQuba">Quba</p>
+                                </div>
+                                <div class="flex gap-2">
+                                    <img src="../assets/images/Squircle.svg" @click="filterSabirabad = !filterSabirabad" v-if="!filterSabirabad" alt="">
+                                    <img src="../assets/images/Checked.svg" @click="filterSabirabad = !filterSabirabad" v-if="filterSabirabad" alt="">
+                                    <p @click="filterSabirabad=!filterSabirabad">Sabirabad</p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -37,12 +96,21 @@
 
 <script>
 
-export default{
-    data(){
-       return{
+export default {
+    data() {
+        return {
 
-           show1:false,
-       } 
+            filterCleaning: false,
+            filterHomeRepair: false,
+            filterBeauty: false,
+            filterBabysitter: false,
+            filterWindowCleaning: false,
+            filterSpecialCare: false,
+            filterBaku: false,
+            filterSumgayit: false,
+            filterQuba: false,
+            filterSabirabad: false,
+        }
     }
 }
 </script>
