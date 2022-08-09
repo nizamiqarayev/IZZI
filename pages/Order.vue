9b2af403f8f79a9e9c8c4d51fcfc4ed6b88c266f
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-09 14:43:25 +0400

    asdasd

--- pages/Order.vue
+++ pages/Order.vue
@@ -1,11 +1,38 @@
 <template>
-    <section class="w-full bg-main ">
+    <section class="w-full font-quicksand bg-main flex h-96 gap-10">
+        <main class="max-w-[65rem] mx-auto">
+            <div class="w-full flex flex-col bg-white border-[1px] rounded-md border-[#C7C9CB1F]">
+                <p class="font-bold pl-6 pt-6">Orders</p>
+                <div @click="selectedMenu = 1" class="hover:cursor-pointer py-4 border-b-[1px] ">
+                    <p class="flex items-center gap-[0.9375rem]">
+                        <img src="../assets/images/Close.svg" alt=""><span>All Services</span>
+                    </p>
+                </div>
+                <div @click="selectedMenu = 2" class="hover:cursor-pointer py-4 border-b-[1px]">
+                    <p class="flex items-center gap-[0.9375rem]">
+                        <img src="../assets/images/Checked.svg" alt=""><span>Completed</span>
+                    </p>
+                </div>
+                <div @click="selectedMenu = 3" class="hover:cursor-pointer py-4 border-b-[1px]">
+                    <p class="flex items-center gap-[0.9375rem]">
+                        <img src="../assets/images/Other.svg" alt=""><span>Cancelled</span>
+                    </p>
+                </div>
+            </div>
+            <div>
 
+            </div>
+        </main>
     </section>
 </template>
 
 <script>
 export default {
 
+    data() {
+        return {
+            selectedMenu: 1,
+        }
+    },
 }
 </script>
