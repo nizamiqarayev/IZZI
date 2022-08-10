051ec033ac9a3f7668602a9d0a794ee4b5ebef6c
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-10 14:06:13 +0400

    asdasd

--- components/ModuleSignUpping.vue
+++ components/ModuleSignUpping.vue
@@ -76,10 +76,8 @@ export default {
     },
     methods: {
         passwordChecker() {
-            if (this.pwfirst == this.pwconfirm && this.pwfirst.length > 7) {
-                const data={''}
-                console.log("amogus");
-                    
+            if (this.pwfirst == this.pwconfirm && this.pwfirst.length>7){
+                    console.log("amogus");
 
             } else {
                 this.pwvalid = false
