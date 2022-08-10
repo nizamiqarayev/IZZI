7b119290e88cd6b143eaa3d2e32c4b913b710521
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-10 14:05:01 +0400

    adad

--- components/ModuleSignUpping.vue
+++ components/ModuleSignUpping.vue
@@ -6,21 +6,24 @@
                 <form action="">
                     <div class="flex flex-col w-full mt-4">
                         <label for="F_name">First Name</label>
-                        <input v-model="f_name" class="mt-3 border-[#C7C9CB] border-[1px] py-3 px-6" placeholder="Firstname" type="text">
+                        <input v-model="f_name" class="mt-3 border-[#C7C9CB] border-[1px] py-3 px-6"
+                            placeholder="Firstname" type="text">
                     </div>
                     <div class="flex flex-col w-full mt-4">
                         <label for="L_name">Last Name</label>
-                        <input v-model="l_name" class="mt-3 border-[#C7C9CB] border-[1px] py-3 px-6" placeholder="Lastname" type="text">
+                        <input v-model="l_name" class="mt-3 border-[#C7C9CB] border-[1px] py-3 px-6"
+                            placeholder="Lastname" type="text">
                     </div>
                     <div class="flex flex-col w-full mt-4">
                         <label for="E-mail">Email address</label>
-                        <input v-model="email" class="mt-3 border-[#C7C9CB] border-[1px] py-3 px-6" placeholder="elonmusk@gmail.com"
-                            type="email">
+                        <input v-model="email" class="mt-3 border-[#C7C9CB] border-[1px] py-3 px-6"
+                            placeholder="elonmusk@gmail.com" type="email">
                     </div>
                     <div class="flex flex-col w-full mt-4 relative">
                         <label for="Password">Password</label>
                         <div class="h-fit flex flex-col items-end">
-                            <input v-model="pwfirst" @keypress="pwvalid = true" :class="pwvalid ? '':'border-red-500 border-2'"
+                            <input v-model="pwfirst" @keypress="pwvalid = true"
+                                :class="pwvalid ? '' : 'border-red-500 border-2'"
                                 class="mt-3 border-[#C7C9CB] w-full border-[1px] py-3 px-6 placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]"
                                 placeholder="12345678" :type="pwhidden ? 'text' : 'password'">
                             <div class="flex justify-end relative bottom-[33px] items-center">
@@ -37,7 +40,8 @@
                     <div class="flex flex-col w-full  relative">
                         <label for="Confirm Password">Confirm Password</label>
                         <div class="h-12 flex flex-col items-end">
-                            <input v-model="pwconfirm" @keypress="pwvalid = true" :class="pwvalid ? '':'border-red-500 border-2'"
+                            <input v-model="pwconfirm" @keypress="pwvalid = true"
+                                :class="pwvalid ? '' : 'border-red-500 border-2'"
                                 class="mt-3 border-[#C7C9CB] w-full border-[1px] py-3 px-6 placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]"
                                 placeholder="12345678" :type="pwhidden ? 'text' : 'password'">
                             <div class="flex justify-end relative bottom-[33px] items-center">
@@ -50,8 +54,10 @@
                             </div>
                         </div>
                     </div>
-                        <button @click="passwordChecker" class="text-base bg-[#5920BC] gap-1 py-3 px-20 my-6 mx-16 rounded-lg  flex items-center gap-2 text-white"  type="button">Sing Up<img
-                                src="../assets/images/signInAndUpImages/arrowright.svg" alt=""></button>
+                    <button @click="passwordChecker"
+                        class="text-base bg-[#5920BC] gap-1 py-3 px-20 my-6 mx-16 rounded-lg  flex items-center gap-2 text-white"
+                        type="button">Sing Up<img src="../assets/images/signInAndUpImages/arrowright.svg"
+                            alt=""></button>
                 </form>
             </div>
         </main>
@@ -59,6 +65,7 @@
 </template>
 
 <script>
+import { axios } from "axios"
 
 export default {
 
@@ -66,20 +73,26 @@ export default {
     data() {
         return {
             pwhidden: false,
-            f_name:"",
-            l_name:"",
-            email:"",
+            f_name: "",
+            l_name: "",
+            email: "",
             pwfirst: "",
             pwconfirm: "",
             pwvalid: true
         }
     },
     methods: {
-        passwordChecker() {
-            if (this.pwfirst == this.pwconfirm && this.pwfirst.length > 7) {
-                const data={''}
-                console.log("amogus");
-                    
+        async passwordChecker() {
+            if (this.pwfirst == this.pwconfirm) {
+                const data = { 'first_name': this.f_name, 'last_name': this.l_name, 'email': this.email, 'password': this.pwfirst }
+                try {
+                    const response =await this.$axios.post('auth/register/', data)
+                    console.log(response);
+                }
+                catch (e) {
+                    console.log(e.message);
+                }
+
 
             } else {
                 this.pwvalid = false
