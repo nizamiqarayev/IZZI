7beab322d5488ede88a1c5c8842af6c97776896b
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-16 15:41:23 +0400

    Order prepearing

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'https://izzi-api-rest.herokuapp.com/api/v1/',
-
+    
   },
   // proxy: {
   //   '/api/': {
--- pages/Order.vue
+++ pages/Order.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="w-full font-quicksand bg-main flex gap-10">
-        <main class="max-w-[65rem] mx-auto flex items-center">
-            <div class="w-full flex-1 flex flex-col p-6 bg-white border-[1px] rounded-md border-[#C7C9CB1F]">
+        <main class="max-w-[65rem] mx-auto flex gap-3">
+            <div class="w-3/12 flex-1 flex flex-col p-6 bg-white border-[1px] rounded-md border-[#C7C9CB1F]">
                 <p class="font-bold">Orders</p>
                 <div @click="selectedMenu = 1" :class="selectedMenu == 1 ? 'border-[#5920BC]' : ''"
                     class="hover:cursor-pointer hover:border-[#5920BC] py-4 border-b-[1px] ">
@@ -22,34 +22,62 @@
                     </p>
                 </div>
             </div>
-            <div  class="bg-white ">
-                <vc-calendar></vc-calendar>
+            <div class="bg-white w-9/12 ">
+                <div class="flex justify-between w-full">
+                    <div class="w-full p-4 flex justify-around">
+                        <div class="flex items-center justify-center">
+                            <img class="h-4" src="../assets/images/search (3).svg" alt="">
+                            <input type="text" placeholder="search" v-model="search">
+                        </div>
+                        <hr class="h-9 border-[1px] w-0">
+                        <div class=" flex items-center justify-center">
+                            <img class="h-4" src="../assets/images/calendar (5).svg" alt="">
+                            <p>Filter dates</p>
+                        </div>
+                    </div>
+                    <div class="px-4 py-2">
+                        <div class="px-4 py-2 w-full gap-2 flex rounded bg-[#5920BC]">
+                            <img src="../assets/images/Add.svg" alt="">
+                            <p class="whitespace-nowrap">Request Now</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     </section>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
 
     data() {
         return {
             selectedMenu: 1,
             range: {
-                start:null,
+                start: null,
                 end: null
-            }
+            },
+            orders: [],
+            search: ''
 
         }
     },
-    mounted(){
-        window.scrollTo(0,0)
+    mounted() {
+        window.scrollTo(0, 0)
     },
-    
-   
-    methods:{
-    
+    async created() {
+        const config = {
+            headers: { Authorization: `${this.$auth.$state.accesslocal}` }
+        }
+        this.orders = await axios.get(`https://izzi-api-rest.herokuapp.com/api/v1/orders/`, config)
+        this.orders = this.orders.data
+    },
+    methods: {
+        message(a) {
+            console.log("message", a);
+        }
     }
 }
+
 </script>
