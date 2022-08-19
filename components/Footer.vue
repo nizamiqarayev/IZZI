0aecfbceb532c79e5334243696a2b02cdbdf7211
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-19 14:04:48 +0400

    Footer responsive fixed

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,47 +1,47 @@
 <template>
     <footer class="font-quicksand w-10/12 mx-auto">
         <hr>
-        <div class="flex justify-between w-full py-12">
-            <div class="px-8 pb-6 w-3/12 space-y-3">
-                <img src="../assets/images/logo/Logo_footer.svg" alt="">
-                <span class="text-[#222222] text-base opacity-75 cursor-pointer">Central street, White City, Khatai, Baku, Azerbaijan</span>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">info@izzi.com</p>
+        <div class="grid grid-cols-2 gap-3 text-xs lg:text-base lg:flex lg:justify-between w-full py-6 lg:py-12">
+            <div class="lg:px-8 lg:pb-6 lg:w-3/12 space-y-3">
+                <img class="h-10 lg:h-14 " src="../assets/images/logo/Logo_footer.svg" alt="">
+                <span class="text-[#222222] opacity-75 cursor-pointer">Central street, White City, Khatai, Baku, Azerbaijan</span>
+                <p class="text-[#222222] opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">info@izzi.com</p>
             </div>
-            <div class="space-y-6">
-                <p class="font-bold">izzi Ltd.</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Location adres</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">info@izzi.com</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">NR21465161</p>
+            <div class="space-y-3 lg:space-y-6">
+                <p class="font-bold text-sm lg:text-xl">izzi Ltd.</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">Location adres</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">info@izzi.com</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">+994 (50) XX XXXX</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">NR21465161</p>
             </div>
-            <div class=" space-y-6">
-                <p class="font-bold">Book a service</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Browse Services</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Find Taskers</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">How does it work?</p>
+            <div class=" space-y-3 lg:space-y-6">
+                <p class="font-bold text-sm lg:text-xl">Book a service</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">Browse Services</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">Find Taskers</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">How does it work?</p>
             </div>
-            <div class=" space-y-6">
-                <p class="font-bold">Extra links</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">About us</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">FAQ</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Contact us</p>
-                <p class="text-[#222222] text-base opacity-75 cursor-pointer">Become a Task</p>
+            <div class=" space-y-3 lg:space-y-6">
+                <p class="font-bold text-sm lg:text-xl">Extra links</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">About us</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">FAQ</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">Contact us</p>
+                <p class="text-[#222222] opacity-75 cursor-pointer">Become a Task</p>
             </div>
         </div>
         <hr>
-        <div class="flex justify-between items-center h-20">
-            <div class="flex gap-12">
+        <div class=" flex flex-col gap-3 mt-3 lg:mt-0 lg:flex-row lg:gap-0 lg:justify-between lg:items-center lg:h-20">
+            <div class="flex gap-6 lg:gap-12">
                 <img src="../assets/images/logo/Facebook_logo.svg" alt="">
                 <img src="../assets/images/logo/Twitter_logo.svg" alt="">
                 <img src="../assets/images/logo/Instagram_logo.svg" alt="">
                 <img src="../assets/images/logo/Youtube_logo.svg" alt="">
             </div>
 
-            <div class="flex justify-around gap-14">
-                <p>izzi Ltd. 2021</p>
-                <p>Privacy Policy</p>
-                <p>Terms and conditions</p>
+            <div class="flex flex-col gap-3 lg:justify-around lg:flex-row lg:gap-14">
+                <p class="text-gray-700 font-semibold">izzi Ltd. 2021</p>
+                <p class="text-gray-700 font-semibold">Privacy Policy</p>
+                <p class="text-gray-700 font-semibold">Terms and conditions</p>
                 <div class="flex gap-4">
                     <img src="../assets/images/logo/GDPR.jpg" alt="">
                     <img src="../assets/images/logo/Trustpilot.jpg" alt="">
