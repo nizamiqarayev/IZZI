ee32a1be2786634fcbb8e80f0d0eba0779c6e4f6
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-21 18:55:55 +0400

    Order date and search fixed

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="w-full font-quicksand bg-main bg-no-repeat flex gap-10">
+    <section class="w-full font-quicksand  bg-main bg-no-repeat flex gap-10">
         <main class="w-9/12 mx-auto mt-[24px] mb-10  text-sm flex gap-3">
             <div class=" w-3/12 flex-1 flex flex-col h-[330px] p-6 bg-white border-[1px] rounded-md border-[#C7C9CB1F]">
                 <p class="font-bold text-lg pl-3">Orders</p>
@@ -44,12 +44,18 @@
                     <div class="p-4 flex gap-2">
                         <div class="flex gap-1 items-center justify-center">
                             <img class="h-4" src="../../assets/images/search (3).svg" alt="">
-                            <input class="px-2 py-2 outline-none" type="text" placeholder="search" v-model="search">
+                            <input class="px-2 py-2 outline-none" type="text" placeholder="search" v-model="searchkey">
                         </div>
                         <hr class="h-9 border-[1px] w-0">
-                        <div class=" flex gap-1 items-center justify-center">
-                            <img class="h-4" src="../../assets/images/calendar (5).svg" alt="">
-                            <p>Filter dates</p>
+                        <div class=" flex gap-1 relative items-center justify-center">
+                            <img @click="showdate = !showdate" class="h-4" src="../../assets/images/calendar (5).svg"
+                                alt="">
+                            <p @click="showdate = !showdate">Filter dates</p>
+                            <div v-if="showdate" class="absolute top-6 h-full w-full">
+                                <vc-date-picker is-range v-model="range" mode="date">
+
+                                </vc-date-picker>
+                            </div>
                         </div>
                     </div>
                     <div class="flex items-center justify-center">
@@ -68,18 +74,17 @@
                     <p class="w-1/12 text-center">Amount</p>
                     <p class="w-1/12 text-center">Order</p>
                 </div>
-                <div class="w-full flex flex-col gap-8 my-4">
+                <div class="w-full flex h-[400px] overflow-y-auto flex-col gap-8 my-4">
                     <div v-for="order in filteredOrders" :key="order.id"
                         class="w-full flex gap-20 justify-around px-3 items-center">
                         <div class="flex w-1/12 items-center">
                             <div class=" flex gap-1 items-center">
-                                <img class="rounded-full h-10 w-10" :src="order.subService.coverPhoto"
-                                    alt="">
+                                <img class="rounded-full h-10 w-10" :src="order.subService.coverPhoto" alt="">
                                 <p>{{ order.subService.title }}</p>
                             </div>
                         </div>
                         <div class="w-1/12 text-center">
-                            <p>{{date(order.startDate)}}</p>
+                            <p>{{ date(order.startDate) }}</p>
                         </div>
                         <div class="w-1/12 text-center">
                             <p>{{ status(order.status) }}</p>
@@ -111,9 +116,10 @@ export default {
                 end: null
             },
             orders: [],
-            search: '',
+            searchkey: '',
             temp_orders: [],
-            
+            showdate: false,
+
 
         }
     },
@@ -132,24 +138,28 @@ export default {
         message(a) {
             console.log("message", a);
         },
-        date(data){
-            let new_date=new Date(data)
-            return new_date.getDate()+"."+(new_date.getMonth()+1)+"."+new_date.getFullYear()
+        date(data) {
+            let new_date = new Date(data)
+            return new_date.getDate() + "." + (new_date.getMonth() + 1) + "." + new_date.getFullYear()
         },
-        status(data){
-            if(data=='accepted'){
+        datefilter(data) {
+            let new_date = new Date(data)
+            return new_date
+        },
+        status(data) {
+            if (data == 'accepted') {
                 return 'Accepted'
             }
-            else if(data=='closed'){
+            else if (data == 'closed') {
                 return 'Cancelled'
             }
-            else if(data=='completed'){
+            else if (data == 'completed') {
                 return 'Completed'
             }
-            else if(data=='inProgress'){
+            else if (data == 'inProgress') {
                 return 'In progress'
             }
-            else if(data=='new'){
+            else if (data == 'new') {
                 return "New"
             }
         },
@@ -158,24 +168,35 @@ export default {
         filteredOrders() {
             this.temp_orders = this.orders
             if (this.selectedMenu == 1) {
-                return this.temp_orders
+                this.temp_orders = this.temp_orders
             } else if (this.selectedMenu == 2) {
-                return this.temp_orders.filter((el) => {
+                this.temp_orders = this.temp_orders.filter((el) => {
                     return el.status == 'accepted'
                 })
             } else if (this.selectedMenu == 3) {
-                return this.temp_orders.filter((el) => {
+                this.temp_orders = this.temp_orders.filter((el) => {
                     return el.status == 'inProgress'
                 })
             } else if (this.selectedMenu == 4) {
-                return this.temp_orders.filter((el) => {
+                this.temp_orders = this.temp_orders.filter((el) => {
                     return el.status == 'completed'
                 })
             } else if (this.selectedMenu == 5) {
-                return this.temp_orders.filter((el) => {
+                this.temp_orders = this.temp_orders.filter((el) => {
                     return el.status == 'closed'
                 })
             }
+            this.temp_orders = this.temp_orders.filter((el) => {
+                if (this.datefilter(el.startDate) > this.range.start && this.datefilter(el.startDate) < this.range.end) {
+                    return true
+                }else if(this.range.start==null||this.range.end==null){
+                    return true
+                }
+            })
+            this.temp_orders = this.temp_orders.filter((el) => {
+                return el.subService.title.toLowerCase().includes(this.searchkey.toLowerCase())
+            })
+            return this.temp_orders
         }
     }
 }
