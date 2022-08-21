10c898d72b11b7053972ed55e953d73d1368fd48
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-08-21 16:54:50 +0400

    SigninFailureDisplay

--- components/ModuleSignIn.vue
+++ components/ModuleSignIn.vue
@@ -1,37 +1,40 @@
 <template>
-  <main class="rounded-[5px] bg-signInAndUpBg w-full bg-contain bg-no-repeat flex items-center justify-center">
+    <main class="rounded-[5px] bg-signInAndUpBg w-full bg-contain bg-no-repeat flex items-center justify-center">
         <div class="font-quicksand mx-[29.1875rem] mt-36 mb-48 p-8 h-fit bg-white border border-solid border-[#E4E4E4]">
-            <h2 class="mr-80 whitespace-nowrap mb-7 font-quicksand font-bold text-[1.875rem] text-[#5920BC]">Sign In</h2>
-                <label for="loginEmail" class="">Email address</label>
-                <input id="loginEmail" type="email" placeholder="elonmusk@gmail.com"
-                    class="mt-3 w-full pl-6 h-12 border border-[#C7C9CB]" v-model="inputEmail" />
-                <label for="loginPW" class="flex justify-between mt-4">
-                    <p>Password</p>
-                    <nuxt-link to="/resetpassword" class="text-[#5920BC]">Forgot password?</nuxt-link>
-                </label>
+            <h2 class="mr-80 whitespace-nowrap mb-7 font-quicksand font-bold text-[1.875rem] text-[#5920BC]">Sign In
+            </h2>
+            <label for="loginEmail" class="">Email address</label>
+            <input id="loginEmail" type="email" placeholder="elonmusk@gmail.com"
+                class="mt-3 w-full pl-6 h-12 border border-[#C7C9CB]" v-model="inputEmail"
+                :class="correct ? 'border-[#C7C9CB]' : 'border-red-600'" />
+            <label for="loginPW" class="flex justify-between mt-4">
+                <p>Password</p>
+                <nuxt-link to="/resetpassword" class="text-[#5920BC]">Forgot password?</nuxt-link>
+            </label>
 
-                <div class="flex flex-col items-end relative">
-                    <input id="loginPW" ref="loginPW" :type="pwhidden ? 'text' : 'password'" 
-                        class="mt-3 w-full pl-6 h-12 flex items-center border border-[#C7C9CB]
-                         placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]"
-                         v-model="inputPW" placeholder="00000000" />
-                    <div class="flex justify-end relative bottom-[33px] items-center">
-                        <div class="mr-4">
-                            <div class="" @click="pwhidden = !pwhidden" v-if="pwhidden == true"><img
-                                    src="../assets/images/signInAndUpImages/eyeclosed.svg" alt=""></div>
-                            <div class="" @click="pwhidden = !pwhidden" v-if="pwhidden == false"><img
-                                    src="../assets/images/signInAndUpImages/eyeopen.svg" alt=""></div>
-                        </div>
-
+            <div class="flex flex-col items-end relative">
+                <input @change="correct = true" id="loginPW" ref="loginPW" :type="pwhidden ? 'text' : 'password'" class="mt-3 w-full pl-6 mb-3 h-12 flex items-center border border-[#C7C9CB]
+                         placeholder:font-password placeholder:text-xs placeholder:text-[#C4C4C4]" v-model="inputPW"
+                    placeholder="00000000" :class="correct ? 'border-[#C7C9CB]' : 'border-red-600'" />
+                <div class="flex justify-end absolute top-[27px] items-center">
+                    <div class="mr-4">
+                        <div class="" @click="pwhidden = !pwhidden" v-if="pwhidden == true"><img
+                                src="../assets/images/signInAndUpImages/eyeclosed.svg" alt=""></div>
+                        <div class="" @click="pwhidden = !pwhidden" v-if="pwhidden == false"><img
+                                src="../assets/images/signInAndUpImages/eyeopen.svg" alt=""></div>
                     </div>
-                    
-
 
                 </div>
-                <div class="flex justify-center items-center">
-                <button @click="signin()" class="shadow-sm rounded-[4px] flex items-center justify-center bg-[#5920BC] text-white py-3 px-28">Sign in <img src="../assets/images/signInAndUpImages/arrowright.svg" alt=""></button>
 
-                </div>
+                <p v-show="correct == false" class="mb-3 text-red-600">Incorrect email or password</p>
+
+            </div>
+            <div class="flex justify-center items-center">
+                <button @click="signin()"
+                    class="shadow-sm rounded-[4px] flex items-center justify-center bg-[#5920BC] text-white py-3 px-28">Sign
+                    in <img src="../assets/images/signInAndUpImages/arrowright.svg" alt=""></button>
+
+            </div>
         </div>
     </main>
 </template>
@@ -42,9 +45,10 @@ export default {
         return {
             pwhidden: false,
             pwplaceholdertext: "",
-            pwplaceholderhidden:false,
+            pwplaceholderhidden: false,
             inputEmail: "",
-            inputPW: ""
+            inputPW: "",
+            correct: true,
         }
     },
     computed: {
@@ -67,7 +71,8 @@ export default {
 
             }
             catch (e) {
-                console.log(e);
+                this.correct = false
+                console.log(e.response);
                 console.log(this.$store.state.auth);
             }
         }
@@ -77,5 +82,4 @@ export default {
 </script>
 
 <style>
-
 </style>
