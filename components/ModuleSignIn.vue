b86c948774c9737cbf22a9e13343a09c260fb66e
Murad <murad.aliyev.13.murad@gmail.com>
2022-08-21 19:19:20 +0400

    Sign-in responsive fixed

--- components/ModuleSignIn.vue
+++ components/ModuleSignIn.vue
@@ -1,7 +1,7 @@
 <template>
-    <main class="rounded-[5px] bg-signInAndUpBg w-full bg-contain bg-no-repeat flex items-center justify-center">
-        <div class="font-quicksand mx-[29.1875rem] mt-36 mb-48 p-8 h-fit bg-white border border-solid border-[#E4E4E4]">
-            <h2 class="mr-80 whitespace-nowrap mb-7 font-quicksand font-bold text-[1.875rem] text-[#5920BC]">Sign In
+    <main class="rounded-[5px] bg-signInAndUpBg w-full lg:bg-contain bg-no-repeat flex items-center justify-center">
+        <div class="font-quicksand w-10/12 lg:w-fit text-sm lg:text-base mx-auto mt-36 mb-48 p-8 h-fit bg-white border border-solid border-[#E4E4E4]">
+            <h2 class="mr-80 whitespace-nowrap mb-7 font-quicksand font-bold text-2xl lg:text-4xl text-[#5920BC]">Sign In
             </h2>
             <label for="loginEmail" class="">Email address</label>
             <input id="loginEmail" type="email" placeholder="elonmusk@gmail.com"
@@ -31,7 +31,7 @@
             </div>
             <div class="flex justify-center items-center">
                 <button @click="signin()"
-                    class="shadow-sm rounded-[4px] flex items-center justify-center bg-[#5920BC] text-white py-3 px-28">Sign
+                    class="shadow-sm rounded-[4px] flex items-center justify-center bg-[#5920BC] text-white py-3 px-11 lg:px-28">Sign
                     in <img src="../assets/images/signInAndUpImages/arrowright.svg" alt=""></button>
 
             </div>
